21dcca6dda255acc5ad7e561d75bb40b8129f91d
marjanamahii <marjanamahi777@gmail.com>
2024-11-09 02:53:05 +0000

    problem 1 done

---
+++ Problem-1.ts
@@ -0,0 +1,12 @@
+{
+  //
+
+  function sumArray(numbers: number[]): number {
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
+
+  // Sample Input
+  console.log(sumArray([1, 2, 3, 4, 5]));
+
+  //
+}
